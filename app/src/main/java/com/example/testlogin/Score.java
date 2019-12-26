5c1f7712284bc66bdfe983ce6322d7590a1088c0
ABDULLAH AZZAM ROBBANI <abdullahazzam0715@gmail.com>
2019-12-26 09:23:01 +0700

    azzam

---
+++ app/src/main/java/com/example/testlogin/Score.java
@@ -0,0 +1,17 @@
+package com.example.testlogin;
+
+public class Score {
+    String score;
+
+    public void setScore (String score)
+    {
+
+        this.score=score;
+    }
+
+    public String getScore()
+    {
+
+        return this.score;
+    }
+}
